692f9c21a7cdb945413faa19986b04e84f7a149d
mwZ101 <marvinwu3@gmail.com>
2023-02-24 11:18:27 -0800

    Modified Patients List Page, such as adding things to the manage test function, adding a search bar to find patients, grouping the search suggestions, making the table show the searched patients, and adding uuid to manually created patients

--- frontend/src/components/patient_list/PatientsTable.js
+++ frontend/src/components/patient_list/PatientsTable.js
@@ -11,12 +11,14 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import { TableVirtuoso, TableComponents } from "react-virtuoso";
 
 import "./PatientsTable.css";
 import AddPatientFullModal from "./AddPatient";
 import ManageTests from "./ManageTests";
 import { useNavigate } from "react-router";
+import SearchForPatients from "./FindPatients";
+
+import { v4 as uuidv4 } from "uuid";
 
 const headerColumns = [
   {
@@ -24,7 +26,11 @@ const headerColumns = [
     className: "header-column-text",
     label: "Patient Name",
   },
-  { id: "patient_id", className: "header-column-text", label: "Patient ID" },
+  { 
+    id: "user_id", 
+    className: "header-column-text", 
+    label: "User ID" 
+  },
   {
     id: "assigned_test_num",
     className: "header-column-text",
@@ -50,222 +56,173 @@ const headerColumns = [
 //change the patient_id values to string type
 const testRows = [
   {
-    patient_id: 19285239,
+    user_id: uuidv4(),
     patient_name: "John Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 65,
   },
   {
-    patient_id: 23897593,
+    user_id: uuidv4(),
     patient_name: "Jane Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 45,
   },
   {
-    patient_id: 90258044,
+    user_id: uuidv4(),
     patient_name: "Robbie Mac",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 56,
   },
   {
-    patient_id: 29235305,
+    user_id: uuidv4(),
     patient_name: "Amanda Spence",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 23,
   },
   {
-    patient_id: 19285239,
+    user_id: uuidv4(),
     patient_name: "John Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 65,
   },
   {
-    patient_id: 23897593,
+    user_id: uuidv4(),
     patient_name: "Jane Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 45,
   },
   {
-    patient_id: 90258044,
+    user_id: uuidv4(),
     patient_name: "Robbie Mac",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 56,
   },
   {
-    patient_id: 29235305,
-    patient_name: "Amanda Spence",
-    assigned_test_num: 1,
-    last_movement_tested: "Sit-to-Stand",
-    last_test_score: "23",
-  },
-  {
-    patient_id: 19285239,
-    patient_name: "John Doe",
-    assigned_test_num: 1,
-    last_movement_tested: "Sit-to-Stand",
-    last_test_score: 65,
-  },
-  {
-    patient_id: 23897593,
-    patient_name: "Jane Doe",
-    assigned_test_num: 1,
-    last_movement_tested: "Sit-to-Stand",
-    last_test_score: 45,
-  },
-  {
-    patient_id: 90258044,
-    patient_name: "Robbie Mac",
-    assigned_test_num: 1,
-    last_movement_tested: "Sit-to-Stand",
-    last_test_score: 56,
-  },
-  {
-    patient_id: 29235305,
+    user_id: uuidv4(),
     patient_name: "Amanda Spence",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 23,
   },
   {
-    patient_id: 19285239,
+    user_id: uuidv4(),
     patient_name: "John Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 65,
   },
   {
-    patient_id: 23897593,
+    user_id: uuidv4(),
     patient_name: "Jane Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 45,
   },
   {
-    patient_id: 90258044,
+    user_id: uuidv4(),
     patient_name: "Robbie Mac",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 56,
   },
   {
-    patient_id: 19285239,
-    patient_name: "John Doe",
-    assigned_test_num: 1,
-    last_movement_tested: "Sit-to-Stand",
-    last_test_score: 65,
-  },
-  {
-    patient_id: 23897593,
-    patient_name: "Jane Doe",
-    assigned_test_num: 1,
-    last_movement_tested: "Sit-to-Stand",
-    last_test_score: 45,
-  },
-  {
-    patient_id: 90258044,
-    patient_name: "Robbie Mac",
-    assigned_test_num: 1,
-    last_movement_tested: "Sit-to-Stand",
-    last_test_score: 56,
-  },
-  {
-    patient_id: 29235305,
+    user_id: uuidv4(),
     patient_name: "Amanda Spence",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 23,
   },
   {
-    patient_id: 19285239,
+    user_id: uuidv4(),
     patient_name: "John Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 65,
   },
   {
-    patient_id: 23897593,
+    user_id: uuidv4(),
     patient_name: "Jane Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 45,
   },
   {
-    patient_id: 90258044,
+    user_id: uuidv4(),
     patient_name: "Robbie Mac",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 56,
   },
   {
-    patient_id: 29235305,
+    user_id: uuidv4(),
     patient_name: "Amanda Spence",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 23,
   },
   {
-    patient_id: 19285239,
+    user_id: uuidv4(),
     patient_name: "John Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 65,
   },
   {
-    patient_id: 23897593,
+    user_id: uuidv4(),
     patient_name: "Jane Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 45,
   },
   {
-    patient_id: 90258044,
+    user_id: uuidv4(),
     patient_name: "Robbie Mac",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 56,
   },
   {
-    patient_id: 29235305,
+    user_id: uuidv4(),
     patient_name: "Amanda Spence",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 23,
   },
   {
-    patient_id: 19285239,
+    user_id: uuidv4(),
     patient_name: "John Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 65,
   },
   {
-    patient_id: 23897593,
+    user_id: uuidv4(),
     patient_name: "Jane Doe",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 45,
   },
   {
-    patient_id: 90258044,
+    user_id: uuidv4(),
     patient_name: "Robbie Mac",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 56,
   },
   {
-    patient_id: 29235305,
+    user_id: uuidv4(),
     patient_name: "Amanda Spence",
     assigned_test_num: 1,
     last_movement_tested: "Sit-to-Stand",
     last_test_score: 23,
-  },
+  }
 ];
 
 function FixedHeaderRow() {
@@ -276,7 +233,7 @@ function FixedHeaderRow() {
           variant="head"
           key={headerColumn.id}
           className={headerColumn.className}
-          sx={{ fontWeight: "bold" }}
+          sx={{ fontWeight: "bold", whiteSpace: 'nowrap' }}
         >
           {headerColumn.label}
         </TableCell>
@@ -285,16 +242,17 @@ function FixedHeaderRow() {
   );
 }
 
-function DisplayRows({ tablePage, rowsPerTablePage, items }) {
+function DisplayRows({ tablePage, rowsPerTablePage, patientDataRowsArr, updatePatientDataRowsArr }) {
   //need to figure out which hooks are needed to make and use
   const [numTestsAssigned, setNumTestsAssigned] = React.useState(0);
   // const [lastMovementTested, setLastMovementTested] = React.useState("-");
   // const [lastTestScore, setLastTestScore] = React.useState("-");
 
-  // const navigate = useNavigate();
   let navigate = useNavigate();
 
-  if (items.length === 0) {
+  console.log("Display All Patients.")
+
+  if (patientDataRowsArr.length === 0) {
     return (
       <TableRow>
         <TableCell colSpan={headerColumns.length} sx={{ textAlign: "center" }}>
@@ -303,8 +261,7 @@ function DisplayRows({ tablePage, rowsPerTablePage, items }) {
       </TableRow>
     );
   } else {
-    return items
-      .slice(
+    return patientDataRowsArr.slice(
         tablePage * rowsPerTablePage,
         tablePage * rowsPerTablePage + rowsPerTablePage
       )
@@ -376,7 +333,13 @@ function DisplayRows({ tablePage, rowsPerTablePage, items }) {
                       {column.label === "Movement Tests"
                         ? obj_value + " Tests Assigned "
                         : obj_value}
-                      {column.label === "Movement Tests" && <ManageTests />}
+                      {column.label === "Movement Tests" && 
+                      <ManageTests 
+                            rowNum={patientDataRowsArr.indexOf(row)} 
+                            user_id={row.user_id} 
+                            patientDataRowsArr={patientDataRowsArr} 
+                            updatePatientDataRowsArr={updatePatientDataRowsArr} 
+                      />}
                     </TableCell>
                   );
                 }
@@ -388,15 +351,121 @@ function DisplayRows({ tablePage, rowsPerTablePage, items }) {
   }
 }
 
+function DisplaySearchResults({ tablePage, rowsPerTablePage, searchResults, setSearchResults, patientDataRowsArr, updatePatientDataRowsArr }) {
+  const [numTestsAssigned, setNumTestsAssigned] = React.useState(0);
+
+  let navigate = useNavigate();
+  console.log("Display Search Results.")
+
+  if (searchResults.length === 0) {
+    return (
+      <TableRow>
+        <TableCell colSpan={headerColumns.length} sx={{ textAlign: "center" }}>
+          No Patients Found - Please Search Another Patient Name.
+        </TableCell>
+      </TableRow>
+    );
+  } else {
+    return searchResults.slice(
+        tablePage * rowsPerTablePage,
+        tablePage * rowsPerTablePage + rowsPerTablePage
+      )
+      .map((row) => {
+        return (
+          <TableRow>
+            {headerColumns.map((column) => {
+              if (column.id === "see_patient_data") {
+                return (
+                  <TableCell
+                    variant="body"
+                    key={column.id}
+                    align="left"
+                    sx={{ whiteSpace: "nowrap" }}
+                  >
+                    {column.id === "see_patient_data" && (
+                      <Button
+                        onClick={() => {
+                          navigate("/patient");
+                        }}
+                      >
+                        See Patient Data
+                      </Button>
+                    )}
+                  </TableCell>
+                );
+              } else {
+                const obj_value = row[column.id];
+
+                if (column.id === "last_test_score") {
+                  return (
+                    <TableCell
+                      variant="body"
+                      key={column.id}
+                      align="left"
+                      sx={() => {
+                        if (obj_value >= 50) {
+                          return {
+                            color: "green",
+                            height: "20px",
+                            whiteSpace: "nowrap",
+                          };
+                        } else if (obj_value >= 0) {
+                          return {
+                            color: "red",
+                            height: "20px",
+                            whiteSpace: "nowrap",
+                          };
+                        } else {
+                          return {
+                            color: "black",
+                            height: "20px",
+                            whiteSpace: "nowrap",
+                          };
+                        }
+                      }}
+                    >
+                      {obj_value}
+                    </TableCell>
+                  );
+                } else {
+                  return (
+                    <TableCell
+                      variant="body"
+                      key={column.id}
+                      align="left"
+                      sx={{ height: "20px", whiteSpace: "nowrap" }}
+                    >
+                      {column.label === "Movement Tests"
+                        ? obj_value + " Tests Assigned "
+                        : obj_value}
+                      {column.label === "Movement Tests" && 
+                      <ManageTests 
+                            rowNum={patientDataRowsArr.indexOf(row)} 
+                            user_id={row.user_id} 
+                            patientDataRowsArr={patientDataRowsArr} 
+                            updatePatientDataRowsArr={updatePatientDataRowsArr} 
+                      />}
+                    </TableCell>
+                  );
+                }
+              }
+            })}
+          </TableRow>
+        );
+      });
+  }
+}
+
+
 export function PatientsTable({ care_provider_id }) {
-  const [items, updateItems] = React.useState([]);
+  let data = testRows;
+
+  const [patientDataRowsArr, updatePatientDataRowsArr] = React.useState(data);
 
   const [tablePage, setTablePage] = React.useState(0);
   const [rowsPerTablePage, setRowsPerTablePage] = React.useState(10);
 
-  React.useEffect(() => {
-    updateItems(testRows);
-  }, [items]);
+  const [searchResults, setSearchResults] = React.useState([]);
 
   const handleChangePage = (event, newTablePage) => {
     setTablePage(newTablePage);
@@ -406,6 +475,11 @@ export function PatientsTable({ care_provider_id }) {
     setRowsPerTablePage(+event.target.value);
     setTablePage(0);
   };
+
+  let paginationCount = patientDataRowsArr.length;
+  if (searchResults.length > 0) {
+    paginationCount = searchResults.length;
+  }
 
   return (
     <Box
@@ -417,7 +491,14 @@ export function PatientsTable({ care_provider_id }) {
       }}
     >
       <div className="above-table-row">
-        <AddPatientFullModal itemsArr={items} updateTableState={updateItems} />
+        <AddPatientFullModal patientDataRowsArr={patientDataRowsArr} updatePatientDataRowsArr={updatePatientDataRowsArr} />
+        <SearchForPatients 
+          patientDataRowsArr={patientDataRowsArr} 
+          updatePatientDataRowsArr={updatePatientDataRowsArr}
+          searchResults={searchResults}
+          setSearchResults={setSearchResults}
+          setTablePage={setTablePage}
+        />
       </div>
 
       <Box sx={{ color: "#000000", width: "100%" }}>
@@ -435,14 +516,15 @@ export function PatientsTable({ care_provider_id }) {
             <TableHead>{FixedHeaderRow()}</TableHead>
 
             <TableBody>
-              {DisplayRows({ tablePage, rowsPerTablePage, items })}
+              {searchResults.length > 0 ? DisplaySearchResults({ tablePage, rowsPerTablePage, searchResults, setSearchResults, patientDataRowsArr, updatePatientDataRowsArr }) :
+               DisplayRows({ tablePage, rowsPerTablePage, patientDataRowsArr, updatePatientDataRowsArr })}
             </TableBody>
           </Table>
 
           <TablePagination
             rowsPerPageOptions={[10, 25, 50]}
             component="div"
-            count={items.length} //change this later
+            count={paginationCount}
             rowsPerPage={rowsPerTablePage}
             page={tablePage}
             onPageChange={handleChangePage}
