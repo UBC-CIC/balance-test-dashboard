c3381398a2464582014208052977583f5d70c454
mwZ101 <marvinwu3@gmail.com>
2023-03-17 11:35:32 -0700

    Made authentication page work

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,5 +1,5 @@
 import { Authenticator, useAuthenticator, useTheme } from '@aws-amplify/ui-react';
-import { Amplify, API, Auth, graphqlOperation } from 'aws-amplify';
+import { Amplify, API, Auth, Hub, graphqlOperation } from 'aws-amplify';
 import React from 'react';
 import { NavBar } from '../components/nav/Navbar';
 import awsconfig from "../aws-exports";
@@ -7,73 +7,73 @@ import { redirect, useNavigate } from "react-router-dom";
 import { PatientsTable } from "../components/patient_list/PatientsTable"
 import Signin from "../components/nav/SignIn";
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
+
 import "./authentication.css";
 
 Amplify.configure(awsconfig);
 
 export default function AuthenticationPage() {
 
+    const [showAccessDenied, setShowAccessDenied] = React.useState(false);
+
+    const handleDeniedClose = () => {
+      setShowAccessDenied(false);
+    }
+
     //to override certain authentication functions
-    // const authServices = {
-    //     async handleSignUp(formData) {
-    //         let { username, password, attributes } = formData;
-    //         console.log("Auth Form Data: ", formData);
+    const authServices = {
+        async handleSignUp(formData) {
+            let { username, password, attributes } = formData;
 
-    //         username = username.toLowerCase(); //username field is email in this case
-    //         attributes.email = attributes.email.toLowerCase();
+            username = username.toLowerCase(); //username field is email in this case
+            attributes.email = attributes.email.toLowerCase();
+            attributes["custom:user_type"] = "care_provider_user";
 
-    //         return Auth.signUp({
-    //           username,
-    //           password,
-    //           attributes,
-    //           autoSignIn: {
-    //             enabled: true,
-    //           },
-    //         });
-    //     },
-    //     async handleSignIn(formData) {
-    //         let { email, password } = formData;
-    //         let allowSignIn = false;
-
-    //         // return Auth.signIn(email, password).then((res) => {
-    //         //     console.log("Sign In response: ", res);
-
-    //         //     let userGroupArr = res["signInUserSession"]["accessToken"]["payload"]["cognito:groups"]
-
-    //         //     if (userGroupArr.includes("care_provider_user")) {
-    //         //         console.log("Is a care provider");
-    //         //         return (
-    //         //             <>
-    //         //                 <PatientsTable careProviderId={1} />
-    //         //             </>
-    //         //         )
-    //         //     } else {
-    //         //         console.log("Not a care provider");
-    //         //     }
-    //         // });
-    //     }
-    // }
+            return Auth.signUp({
+              username,
+              password,
+              attributes,
+              autoSignIn: {
+                enabled: true,
+              },
+            });
+        }
+    }
 
     return (
-        // <Authenticator services={authServices}>
-        <Authenticator>
-            {({ signOut, user}) => {
+      <Box>
+          <Authenticator services={authServices}>
+              {({ signOut, user}) => {
 
-                // console.log(user);
+                  console.log("User: ", user);
 
-                let userGroupArr = user["signInUserSession"]["accessToken"]["payload"]["cognito:groups"]
+                  let userGroupArr = user["signInUserSession"]["accessToken"]["payload"]["cognito:groups"];
+                  let user_id = user['username'];
 
-                // if (userGroupArr.includes("care_provider_user")) {
-                return (
-                    <>
-                        <PatientsTable careProviderId={1} />
-                        <button onClick={signOut}>Sign out</button>
-                    </>
-                )
-                // } else {
-                //     return signOut()
-                // }
-            }}
-        </Authenticator>
+                  if (userGroupArr.includes("care_provider_user")) {
+                      return (
+                          <>
+                              <PatientsTable careProviderId={user_id} />
+                              <button onClick={signOut}>Sign out</button>
+                          </>
+                      )
+                  } else {
+                      Auth.signOut(); 
+                      setShowAccessDenied(true);
+                  }
+              }}
+          </Authenticator>
+          <Snackbar
+            open={showAccessDenied}
+            autoHideDuration={5000}
+            anchorOrigin = {{'horizontal': 'center', 'vertical': 'bottom'}}
+            onClose={handleDeniedClose}
+            message="No Permissions to Access."
+          />
+      </Box>
     )
 }
