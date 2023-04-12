2db5be1bec4fe0e56464d33a9e9b33de4758ad9f
Fu <mminting@ead.ubc.ca>
2023-04-12 09:54:58 -0700

    Modify amplify build settings in amplify.yml

--- backend/cdk/lib/cognito-stack.ts
+++ backend/cdk/lib/cognito-stack.ts
@@ -4,6 +4,7 @@ import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as logs from "aws-cdk-lib/aws-logs";
 import * as ssm from "aws-cdk-lib/aws-ssm";
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 import * as cdk from 'aws-cdk-lib';
 import { VPCStack } from './vpc-stack';
 import { DataWorkflowStack } from './data-workflow-stack';
@@ -15,212 +16,208 @@ export class CognitoStack extends Stack {
     constructor(scope: App, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const appsyncName = "BalanceTest-API";
-
-        const generateReportLambdaDataSourceName = "BalanceTest_generate_report_s3_lambda";
-        const postgresqlRDSLambdaDataSourceName = "BalanceTest_postgresql_connection_lambda";
-        const queryS3LambdaDataSourceName = "BalanceTest_query_s3_lambda";
-        const deleteEventLambdaDataSourceName = "BalanceTest_delete_event_record_lambda";
-
-        const generateReportDataSourceRoleName = "BalanceTest-appsync-generateReportDataSource-Role";
-        const postgresqlRDSConnectDataSourceRoleName = "BalanceTest-appsync-postgresqlRDSConnectDataSource-Role";
-        const queryS3DataSourceRoleName = "BalanceTest-appsync-queryS3DataSource-Role";
-        const deleteEventDataSourceRoleName = "BalanceTest-appsync-deleteEventDataSource-Role";
-        
-        //TODO: get graphql api ID from Parameter Store
-        const apiId = ssm.StringParameter.fromStringParameterAttributes(this, "BalanceTestAppsyncApiId", {
-            parameterName: "GraphqlApiId",
-        }).stringValue;
-
-        const api = appsync.GraphqlApi.fromGraphqlApiAttributes(this, appsyncName, {
-            graphqlApiId: apiId
+        const lambdaRole = new iam.Role(this, 'HealthPlatformCognitoLambdaRole', {
+            roleName: 'HealthPlatformCognitoLambdaRole',
+            assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+            inlinePolicies: {
+                additional: new PolicyDocument({
+                        statements: [
+                        new PolicyStatement({
+                            effect: Effect.ALLOW,
+                            actions: [
+                                // DynamoDB
+                                'dynamodb:Scan',
+                                'dynamodb:GetItem',
+                                'dynamodb:PutItem',
+                                'dynamodb:Query',
+                                'dynamodb:UpdateItem',
+                                'dynamodb:DeleteItem',
+                                'dynamodb:BatchWriteItem',
+                                'dynamodb:BatchGetItem',
+                                // Lambda
+                                'lambda:InvokeFunction',
+                                // CloudWatch
+                                'cloudwatch:*',
+                                'logs:*',
+                                //SSM
+                                'ssm:*',
+                            ],
+                            resources: ['*']
+                        })
+                    ]
+                }),
+            },
         });
 
-        // getting needed Lambda functions for data sources
-        const generateReportLambda = dataWorkflowStack.getGenerateReportLambda();
-        const postgresqlRDSConnectLambda = databaseStack.getPostgresqlRDSConnectLambda();
-        const athenaQueryS3Lambda = athenaGlueStack.getAthenaS3QueryLambda();
-        const deleteEventLambda = dataWorkflowStack.getDeleteS3RecordLambda();
-
-        // making IAM roles for data sources
-        const generateReportDataSourcePolicyDocument = new iam.PolicyDocument({
-            statements: [new iam.PolicyStatement({
-            actions: ["lambda:invokeFunction"],
-            resources: [generateReportLambda.functionArn]
-            })]
-        });
-        const generateReportDataSourceRole = new iam.Role(this, generateReportDataSourceRoleName, {
-            assumedBy: new iam.ServicePrincipal("appsync.amazonaws.com"),
-            roleName: generateReportDataSourceRoleName,
-            inlinePolicies: { ["BalanceTest-generateReportDataSourcePolicy"]: generateReportDataSourcePolicyDocument }
+        const createPatientsDetailFunction = new lambda.Function(this, 'CreatePatientsDetailFunction', {
+            functionName: "Patients-Detail-Create",
+            code: new lambda.AssetCode('build/src'),
+            handler: 'patients-detail-create.handler',
+            runtime: lambda.Runtime.NODEJS_12_X,
+            memorySize: 512,
+            timeout: cdk.Duration.seconds(30),
+            role: lambdaRole,
         });
 
-        const postgresqlRDSConnectDataSourcePolicyDocument = new iam.PolicyDocument({
-            statements: [new iam.PolicyStatement({
-            actions: ["lambda:invokeFunction"],
-            resources: [postgresqlRDSConnectLambda.functionArn]
-            })]
+        // User Pool
+        const userPool = new cognito.UserPool(this, 'BalanceTestUserPool', {
+            userPoolName: 'balance-test-user-pool',
+            selfSignUpEnabled: true,
+            userVerification: {
+              emailSubject: 'Verify your email for Balance Test!',
+              emailBody: 'Your verification code is {####}',
+            },
+            signInAliases: {
+                email: true,
+                phone: false
+            },
+            autoVerify: { 
+                email: true
+            },
+            standardAttributes: {
+                // TODO: Add fullname in the future
+                familyName:{
+                    required: true,
+                    mutable: true
+                },
+                givenName:{
+                    required: true,
+                    mutable: true
+                },
+            },
+            customAttributes: {
+                'joinedOn': new DateTimeAttribute(),
+            },
+            accountRecovery: AccountRecovery.EMAIL_ONLY,
+            lambdaTriggers: {
+                postConfirmation: createPatientsDetailFunction,
+            }
         });
-        const postgresqlRDSConnectDataSourceRole = new iam.Role(this, postgresqlRDSConnectDataSourceRoleName, {
-            assumedBy: new iam.ServicePrincipal("appsync.amazonaws.com"),
-            roleName: postgresqlRDSConnectDataSourceRoleName,
-            inlinePolicies: { ["BalanceTest-postgresqlRDSConnectDataSourcePolicy"]: postgresqlRDSConnectDataSourcePolicyDocument }
-        });
+        this.UserPoolId = userPool.userPoolId;
 
-        const queryS3DataSourcePolicyDocument = new iam.PolicyDocument({
-            statements: [new iam.PolicyStatement({
-            actions: ["lambda:invokeFunction"],
-            resources: [athenaQueryS3Lambda.functionArn]
-            })]
-        });
-        const queryS3DataSourceRole = new iam.Role(this, queryS3DataSourceRoleName, {
-            assumedBy: new iam.ServicePrincipal("appsync.amazonaws.com"),
-            roleName: queryS3DataSourceRoleName,
-            inlinePolicies: { ["BalanceTest-queryS3DataSourcePolicy"]: queryS3DataSourcePolicyDocument }
-        });
-
-        const deleteEventDataSourcePolicyDocument = new iam.PolicyDocument({
-            statements: [new iam.PolicyStatement({
-            actions: ["lambda:invokeFunction"],
-            resources: [deleteEventLambda.functionArn]
-            })]
-        });
-        const deleteEventDataSourceRole = new iam.Role(this, deleteEventDataSourceRoleName, {
-            assumedBy: new iam.ServicePrincipal("appsync.amazonaws.com"),
-            roleName: deleteEventDataSourceRoleName,
-            inlinePolicies: { ["BalanceTest-deleteEventDataSourcePolicy"]: deleteEventDataSourcePolicyDocument }
-        });
-        
-        //making data sources from the Lambda functions
-        const generateReportLambdaDataSource = new appsync.LambdaDataSource(this, generateReportLambdaDataSourceName, {
-            api: api,
-            lambdaFunction: generateReportLambda,
-            name: generateReportLambdaDataSourceName,
-            serviceRole: generateReportDataSourceRole
-        });
-
-        const postgresqlRDSConnectLambdaDataSource = new appsync.LambdaDataSource(this, postgresqlRDSLambdaDataSourceName, {
-            api: api,
-            lambdaFunction: postgresqlRDSConnectLambda,
-            name: postgresqlRDSLambdaDataSourceName,
-            serviceRole: postgresqlRDSConnectDataSourceRole
+        // User Pool Client
+        const userPoolClient = new CfnUserPoolClient(this, 'HealthPlatformUserPoolClient', {
+            clientName: 'HealthPlatformUserPoolClient',
+            userPoolId: userPool.userPoolId,
+            explicitAuthFlows: [
+                "ALLOW_USER_SRP_AUTH",
+                "ALLOW_REFRESH_TOKEN_AUTH"
+            ]
         });
 
-        const queryS3LambdaDataSource = new appsync.LambdaDataSource(this, queryS3LambdaDataSourceName, {
-            api: api,
-            lambdaFunction: athenaQueryS3Lambda,
-            name: queryS3LambdaDataSourceName,
-            serviceRole: queryS3DataSourceRole
+        // Identity Pool
+        const identityPool = new CfnIdentityPool(this, 'HealthPlatformIdentityPool', {
+            allowUnauthenticatedIdentities: false,
+            cognitoIdentityProviders: [{
+                clientId: userPoolClient.ref,
+                providerName: userPool.userPoolProviderName
+            }]
         });
 
-        const deleteEventLambdaDataSource = new appsync.LambdaDataSource(this, deleteEventLambdaDataSourceName, {
-            api: api,
-            lambdaFunction: deleteEventLambda,
-            name: deleteEventLambdaDataSourceName,
-            serviceRole: deleteEventDataSourceRole
+        // Unauthenticated Role
+        const unauthenticatedRole = new Role(
+            this,
+            "HealthPlatform_Website_Unauthenticated_Role",
+            {
+                roleName: "HealthPlatform_Website_Unauthenticated_Role",
+                assumedBy: new FederatedPrincipal(
+                    "cognito-identity.amazonaws.com",
+                    {
+                        StringEquals: {
+                            "cognito-identity.amazonaws.com:aud": identityPool.ref
+                        },
+                        "ForAnyValue:StringLike": {
+                            "cognito-identity.amazonaws.com:amr": "unauthenticated"
+                        }
+                    },
+                    "sts:AssumeRoleWithWebIdentity"
+                )
+            },
+        );
+
+        // Authenticated Role
+        const authenticatedRole = new Role(
+            this,
+            "HealthPlatform_Website_Authenticated_Role",
+            {
+                roleName: "HealthPlatform_Website_Authenticated_Role",
+                assumedBy: new FederatedPrincipal(
+                    "cognito-identity.amazonaws.com",
+                    {
+                        StringEquals: {
+                            "cognito-identity.amazonaws.com:aud": identityPool.ref
+                        },
+                        "ForAnyValue:StringLike": {
+                            "cognito-identity.amazonaws.com:amr": "authenticated"
+                        }
+                    },
+                    "sts:AssumeRoleWithWebIdentity"
+                )
+            }
+        );
+        authenticatedRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AWSAppSyncInvokeFullAccess'))
+
+        // Identity Pool Role Attachment
+        new CfnIdentityPoolRoleAttachment(
+            this,
+            "HealthPlatformIdentityPoolRoleAttachment",
+            {
+                identityPoolId: identityPool.ref,
+                roles: {
+                    unauthenticated: unauthenticatedRole.roleArn,
+                    authenticated: authenticatedRole.roleArn
+                }
+            }
+        );
+
+        // outputs
+        new CfnOutput(this, 'UserPoolId', {
+            value: userPool.userPoolId
         });
 
-        //TODO: finish making functions
-        const s3DownloadAppsyncFunctionName = "redirect_s3_download_function";
-        const s3DataRetrievalAppsyncFunctionName = "redirect_s3_data_retrieval_function";
-        const postgresqlRDSAppsyncFunctionName = "redirect_postgresql_rds_function";
-        const deleteEventAppsyncFunctionName = "redirect_delete_event_function";
-
-        const s3DownloadAppsyncFunction = new appsync.AppsyncFunction(this, s3DownloadAppsyncFunctionName, {
-            api: api,
-            name: s3DownloadAppsyncFunctionName,
-            dataSource: generateReportLambdaDataSource,
-            runtime: appsync.FunctionRuntime.JS_1_0_0,
-            code: appsync.Code.fromAsset('appsync/AppsyncFunctions/redirect_s3_download_function.js'),
+        new CfnOutput(this, 'UserPoolClientId', {
+            value: userPoolClient.ref
         });
 
-        const s3DataRetrievalAppsyncFunction = new appsync.AppsyncFunction(this, s3DataRetrievalAppsyncFunctionName, {
-            api: api,
-            name: s3DataRetrievalAppsyncFunctionName,
-            dataSource: queryS3LambdaDataSource,
-            runtime: appsync.FunctionRuntime.JS_1_0_0,
-            code: appsync.Code.fromAsset('appsync/AppsyncFunctions/redirect_s3_data_retrieval_function.js'),
+        new CfnOutput(this, 'IdentityPoolId', {
+            value: identityPool.ref
         });
 
-        const postgresqlRDSAppsyncFunction = new appsync.AppsyncFunction(this, postgresqlRDSAppsyncFunctionName, {
-            api: api,
-            name: postgresqlRDSAppsyncFunctionName,
-            dataSource: postgresqlRDSConnectLambdaDataSource,
-            runtime: appsync.FunctionRuntime.JS_1_0_0,
-            code: appsync.Code.fromAsset('appsync/AppsyncFunctions/redirect_postgresql_rds_function.js'),
+        new CfnOutput(this, "AuthenticatedRole", {
+            value: authenticatedRole.roleArn,
         });
-        
-        const deleteEventAppsyncFunction = new appsync.AppsyncFunction(this, deleteEventAppsyncFunctionName, {
-            api: api,
-            name: deleteEventAppsyncFunctionName,
-            dataSource: deleteEventLambdaDataSource,
-            runtime: appsync.FunctionRuntime.JS_1_0_0,
-            code: appsync.Code.fromAsset('appsync/AppsyncFunctions/redirect_delete_event_function.js'),
+
+        new CfnOutput(this, "UnauthenticatedRole", {
+            value: unauthenticatedRole.roleArn,
         });
-        
-        //TODO: update the appsync/resolverMappingFunctions folder with any new queries/mutations
-        // make resolvers by looping through each list and getting the associated code
-        let deleteEventResolverNameList = ["deleteTestEventFromS3"];
-        let s3DownloadResolverNameList = ["downloadTestEventDetails"];
-        let s3DataRetrievalResolverNameList = ['getMeasurementRange','getMeasurementData'];
 
-        let postgresqlRDSQueryResolverNameList = ['getAllAvailableTests', 'getAllPatients', "getCareproviderById", 'getPatientAssignedTests', 'getPatientById', 'getPatientsForCareprovider', 'getScoreStatsOverTime', 'getTestEventById', 'getTestEvents'];
-        let postgresqlRDSMutationResolverNameList = ['addPatientToCareProvider', 'addTestType', 'assignTestToPatient', 'createCareProvider', 'createPatient', 'deleteTestEventFromDB', 'putBalanceScore', 'putTestResult', 'recordConsentDate', 'removeTestFromPatient'];
+        new ssm.StringParameter(this, 'CognitoUserPoolId', {
+            description: 'Cognito User Pool Id',
+            parameterName: 'CognitoUserPoolId',
+            stringValue: this.UserPoolId
+        }); 
 
-        //TODO: double check overall Appsync flow & configuration and if the resolvers work on console
-        for (let i = 0; i < deleteEventResolverNameList.length; i++) {
-            let deleteEventResolver = new appsync.Resolver(this, deleteEventResolverNameList[i], {
-                api: api,
-                fieldName: deleteEventResolverNameList[i],
-                typeName: "Mutation",
-                runtime: appsync.FunctionRuntime.JS_1_0_0,
-                code: appsync.Code.fromAsset('appsync/resolverMappingFunctions/' + deleteEventResolverNameList[i] + ".js"),
-                pipelineConfig: [deleteEventAppsyncFunction],
-            });
-        }
+        new ssm.StringParameter(this, 'CognitoUserPoolClientId', {
+            description: 'User Pool Client Id',
+            parameterName: 'UserPoolClientId',
+            stringValue: userPoolClient.ref
+        }); 
 
-        for (let i = 0; i < s3DownloadResolverNameList.length; i++) {
-            let s3DownloadResolver = new appsync.Resolver(this, s3DownloadResolverNameList[i], {
-                api: api,
-                fieldName: s3DownloadResolverNameList[i],
-                typeName: "Query",
-                runtime: appsync.FunctionRuntime.JS_1_0_0,
-                code: appsync.Code.fromAsset('appsync/resolverMappingFunctions/' + s3DownloadResolverNameList[i] + ".js"),
-                pipelineConfig: [s3DownloadAppsyncFunction],
-            });
-        }
+        new ssm.StringParameter(this, 'CognitoIdentityPoolId', {
+            description: 'Identity Pool Id',
+            parameterName: 'IdentityPoolId',
+            stringValue: identityPool.ref
+        }); 
 
-        for (let i = 0; i < s3DataRetrievalResolverNameList.length; i++) {
-            let s3DataRetrievalResolver = new appsync.Resolver(this, s3DataRetrievalResolverNameList[i], {
-                api: api,
-                fieldName: s3DataRetrievalResolverNameList[i],
-                typeName: "Query",
-                runtime: appsync.FunctionRuntime.JS_1_0_0,
-                code: appsync.Code.fromAsset('appsync/resolverMappingFunctions/' + s3DataRetrievalResolverNameList[i] + ".js"),
-                pipelineConfig: [s3DataRetrievalAppsyncFunction],
-            });
-        }
+        new ssm.StringParameter(this, 'CognitoUnauthenticatedRole', {
+            description: 'Unauthenticated Role',
+            parameterName: 'UnauthenticatedRole',
+            stringValue: unauthenticatedRole.roleArn
+        }); 
 
-        for (let i = 0; i < postgresqlRDSQueryResolverNameList.length; i++) {
-            let postgresqlRDSQueryResolver = new appsync.Resolver(this, postgresqlRDSQueryResolverNameList[i], {
-                api: api,
-                fieldName: postgresqlRDSQueryResolverNameList[i],
-                typeName: "Query",
-                runtime: appsync.FunctionRuntime.JS_1_0_0,
-                code: appsync.Code.fromAsset('appsync/resolverMappingFunctions/' + postgresqlRDSQueryResolverNameList[i] + ".js"),
-                pipelineConfig: [postgresqlRDSAppsyncFunction],
-            });
-        }
-
-        for (let i = 0; i < postgresqlRDSMutationResolverNameList.length; i++) {
-            let postgresqlRDSMutationResolver = new appsync.Resolver(this, postgresqlRDSMutationResolverNameList[i], {
-                api: api,
-                fieldName: postgresqlRDSMutationResolverNameList[i],
-                typeName: "Mutation",
-                runtime: appsync.FunctionRuntime.JS_1_0_0,
-                code: appsync.Code.fromAsset('appsync/resolverMappingFunctions/' + postgresqlRDSMutationResolverNameList[i] + ".js"),
-                pipelineConfig: [postgresqlRDSAppsyncFunction],
-            });
-        }
-    }
+        new ssm.StringParameter(this, 'CognitoAuthenticatedRole', {
+            description: 'Authenticated Role',
+            parameterName: 'AuthenticatedRole',
+            stringValue: authenticatedRole.roleArn
+        });     }
 }
