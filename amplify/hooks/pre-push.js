a1a9d00a8344cd28a5374911c6654c5230af842d
Fu <mminting@ead.ubc.ca>
2023-04-23 11:06:06 -0700

    Try adding prebuild script to amplify

--- amplify/hooks/pre-push.js
+++ amplify/hooks/pre-push.js
@@ -1,15 +1,15 @@
-const fs = require("fs");
-const region = process.env.REGION;
-const authParametersPath =
-  "./amplify/backend/auth/balancetestdashboardf52840bc/parameters.json";
-const parameters = JSON.parse(
-  fs.readFileSync(authParametersPath, {
-    encoding: "utf8",
-  })
-);
+// const fs = require("fs");
+// const region = process.env.REGION;
+// const authParametersPath =
+//   "./amplify/backend/auth/balancetestdashboardf52840bc/parameters.json";
+// const parameters = JSON.parse(
+//   fs.readFileSync(authParametersPath, {
+//     encoding: "utf8",
+//   })
+// );
 
-parameters.region = process.env.REGION;
+// parameters.region = process.env.REGION;
 
-console.log("rewriting auth region");
-fs.writeFileSync(authParametersPath, JSON.stringify(parameters, null, 2));
-console.log("rewrite succeeded");
+// console.log("rewriting auth region");
+// fs.writeFileSync(authParametersPath, JSON.stringify(parameters, null, 2));
+// console.log("rewrite succeeded");
